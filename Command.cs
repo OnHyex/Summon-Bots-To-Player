1d823f7ff9a7942c0ca651ad2d5e3ab97e73e8da
OnHyex <100549656+OnHyex@users.noreply.github.com>
2025-03-15 22:18:13 -0400

    Initial commit

---
+++ Command.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ExitGames.Demos.DemoAnimator;
+using PulsarModLoader.Chat.Commands.CommandRouter;
+using PulsarModLoader.Utilities;
+using UnityEngine;
+
+namespace Teleport_Bots_to_Player
+{
+    internal class Command : ChatCommand
+    {
+        public override string[] CommandAliases()
+        {
+            return new string[] { "summonbots" };
+        }
+        public override string Description()
+        {
+            return "summonbots to player";
+        }
+        public override void Execute(string arguments)
+        {
+            PLPlayer MyPLayer = PLNetworkManager.Instance.LocalPlayer;
+            if (!PhotonNetwork.isMasterClient)
+            {
+                Messaging.Echo(MyPLayer, "Not allowed to use mod if not host");
+                return;
+            }
+            foreach (PLPlayer player in PLServer.Instance.AllPlayers)
+            {
+                if (player != null && player.TeamID == 0 && player.IsBot)
+                {
+                    Vector3 position = MyPLayer.GetPawn().transform.position;
+                    player.photonView.RPC("NetworkTeleportToSubHub", PhotonTargets.All, new object[]
+                    {
+                            MyPLayer.MyCurrentTLI.SubHubID,
+                            0
+                    });
+                    player.photonView.RPC("RecallPawnToPos", PhotonTargets.All, new object[] { position });
+                }
+            }
+        }
+    }
+}
+
---
+++ Mod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PulsarModLoader;
+using PulsarModLoader.MPModChecks;
+
+namespace Teleport_Bots_to_Player
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "1.0.0";
+        public override string Author => "OnHyex";
+        public override string LongDescription => "Mod has command to teleport bots to player";
+        public override string Name => "SummonBots";
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+        public override int MPRequirements => (int)MPRequirement.None;
+    }
+}
