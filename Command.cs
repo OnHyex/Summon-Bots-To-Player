ebeb200daecff988280cc365c8b94af4dab2c171
OnHyex <100549656+OnHyex@users.noreply.github.com>
2025-03-17 15:22:56 -0400

    Update from template mod to actual funcitonal mod based on postfix method of handle movement
    Could still use binding into tickclosetocaptain to only teleport bots that actually are currently following captain but eh.

--- Command.cs
+++ Command.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using ExitGames.Demos.DemoAnimator;
-using PulsarModLoader.Chat.Commands.CommandRouter;
+﻿using PulsarModLoader.Chat.Commands.CommandRouter;
 using PulsarModLoader.Utilities;
 using UnityEngine;
+using HarmonyLib;
 
 namespace Teleport_Bots_to_Player
 {
@@ -18,23 +17,65 @@ namespace Teleport_Bots_to_Player
         }
         public override void Execute(string arguments)
         {
-            PLPlayer MyPLayer = PLNetworkManager.Instance.LocalPlayer;
             if (!PhotonNetwork.isMasterClient)
             {
-                Messaging.Echo(MyPLayer, "Not allowed to use mod if not host");
+                PLPlayer MyPlayer = PLNetworkManager.Instance.LocalPlayer;
+                Messaging.Echo(MyPlayer, "Not allowed to use mod if not host");
                 return;
             }
+            SummonBotFix.doTeleport = true;
+            SummonBotFix.botsToTeleport = 0;
             foreach (PLPlayer player in PLServer.Instance.AllPlayers)
             {
                 if (player != null && player.TeamID == 0 && player.IsBot)
                 {
-                    Vector3 position = MyPLayer.GetPawn().transform.position;
-                    player.photonView.RPC("NetworkTeleportToSubHub", PhotonTargets.All, new object[]
+                    SummonBotFix.botsToTeleport++;
+                }
+            }
+        }
+    }
+    //Potentially just hijack the bot spawning function with a prefix to spawn the bots in the correct location with a command to remove them
+    class SummonBotFix
+    {
+        public static bool doTeleport = false;
+        public static int botsToTeleport = 0;
+        [HarmonyPatch(typeof(PLBotController), "HandleMovement")]
+        internal class HandleMovementFix
+        {
+            private static void Postfix(PLBotController __instance)
+            {
+                PLPlayer Hostplayer = PLNetworkManager.Instance.LocalPlayer;
+                if (doTeleport && botsToTeleport > 0 && __instance != null && __instance.MyPawn != null && !__instance.MyPawn.IsDead && __instance.MyPawn.TeamID == 0 && Hostplayer.GetPawn() != null && (!Hostplayer.GetPawn().SpawnedInArena || !Hostplayer.GetPawn().IsDead))
+                {
+                    Vector3 vector = Hostplayer.GetPawn().transform.position;
+                    __instance.MyBot.AI_TargetTLI = Hostplayer.MyCurrentTLI;
+                    int ttiid = PLBotController.GetTargetTTIIDFromTargetPos(__instance.MyBot.AI_TargetTLI, vector);
+                    if (ttiid == -1)
                     {
-                            MyPLayer.MyCurrentTLI.SubHubID,
-                            0
-                    });
-                    player.photonView.RPC("RecallPawnToPos", PhotonTargets.All, new object[] { position });
+                        ttiid = 0;
+                    }
+                    __instance.AIPaused = false;
+                    __instance.Path = null;
+                    __instance.MyPawn.MyInterior = Hostplayer.CurrentInterior;
+                    __instance.MyPawn.MyPlayer.CurrentInterior = Hostplayer.CurrentInterior;
+                    __instance.MyPawn.MyPlayer.SetSubHubAndTTIID(__instance.MyBot.AI_TargetTLI.SubHubID, ttiid);
+                    __instance.MyPawn.transform.position = vector;
+                    Physics.SyncTransforms();
+                    __instance.MyPawn.CurrentShip = Hostplayer.GetPawn().CurrentShip;
+                    __instance.MyPawn.SpawnedInArena = Hostplayer.GetPawn().SpawnedInArena;
+                    __instance.MyPawn.OnTeleport();
+                    if (__instance.MyPawn != null)
+                    {
+                        __instance.MyBot.AI_TargetPos = __instance.MyPawn.transform.position;
+                        __instance.MyBot.AI_TargetPos_Raw = __instance.MyBot.AI_TargetPos;
+                        __instance.MyBot.ResetTLI();
+                    }
+                    botsToTeleport--;
+                    if (botsToTeleport == 0) //technically doTeleport is uneeded entirely and botsToTeleport is all that is required but leaving in for safety
+                    {
+                        doTeleport = false;
+                    }
+                    return;
                 }
             }
         }
