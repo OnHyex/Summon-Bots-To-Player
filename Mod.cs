73ee23c0b59726fdaf0fb01bf5f3af6a8cf7bc51
OnHyex <100549656+OnHyex@users.noreply.github.com>
2025-03-17 21:18:23 -0400

    Makes it so you can load into game with Mod loaded

--- Mod.cs
+++ Mod.cs
@@ -6,7 +6,7 @@ namespace SummonBotsToPlayer
 {
     public class Mod : PulsarMod
     {
-        public override string Version => "1.0.0";
+        public override string Version => "1.0.1";
         public override string Author => "OnHyex";
         public override string ShortDescription => "Summon bots to whomever runs the !summonbots command";
         public override string Name => "SummonBots";
@@ -14,7 +14,7 @@ namespace SummonBotsToPlayer
         {
             return $"{Author}.{Name}";
         }
-        public override int MPRequirements => (int)MPRequirement.Host;
+        public override int MPRequirements => (int)MPRequirement.None;
         public override string License => "GNU GPL-3.0";
     }
 }
